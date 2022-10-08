dfc6d04b68515115d6883dc6fa5c02e3d2dd07b7
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-08 14:31:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,21 @@
 # ******************************
 # Make your Code
 # ******************************
+numbers = []
+total = 0
+
+# taking user input
+for i in range(5):
+    num = int(input())
+    numList.append(num)
+
+# processing the list
+minNum = min(numList)
+maxNum = max(numList)
+
+for n in numList:
+    total += n
+
+total = total - (minNum + maxNum)
+
+print(total)
