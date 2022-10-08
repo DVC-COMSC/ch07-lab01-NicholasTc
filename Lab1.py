58e2247ae668895390df765bb610efc8fa1f093f
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-08 14:28:40 -0700

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,18 @@
+numList = []
+total = 0
+
+# taking user input
+for i in range(5):
+    num = int(input())
+    numList.append(num)
+
+# processing the list
+minNum = min(numList)
+maxNum = max(numList)
+
+for n in numList:
+    total += n
+
+total = total - (minNum + maxNum)
+
+print(total)
